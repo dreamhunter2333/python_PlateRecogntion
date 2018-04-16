d4ea27c273e6deb635c06365821422edb03dfbe7
yinghualuowu <1048146365@qq.com>
2018-04-16 10:59:18 +0800

    更新一个功能，用于缩小图像边缘

--- img_math.py
+++ img_math.py
@@ -2,6 +2,8 @@
 __author__ = '樱花落舞'
 import cv2
 import numpy as np
+import debug
+
 MAX_WIDTH = 1000
 Min_Area = 2000
 """
@@ -11,6 +13,8 @@ Min_Area = 2000
 颜色判断函数
 """
 def img_read(filename):
+
+
     return  cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
     #以uint8方式读取filename 放入imdecode中，cv2.IMREAD_COLOR读取彩色照片
 
@@ -20,41 +24,72 @@ def point_limit(point):
     if point[1] < 0:
         point[1] = 0
 
+def accurate_place(card_img_hsv, limit1, limit2, color):
+    row_num, col_num = card_img_hsv.shape[:2]
+    xl = col_num
+    xr = 0
+    yh = 0
+    yl = row_num
+    row_num_limit = 21
+    col_num_limit = col_num * 0.8 if color != "green" else col_num * 0.5  # 绿色有渐变
+    for i in range(row_num):
+        count = 0
+        for j in range(col_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > col_num_limit:
+            if yl > i:
+                yl = i
+            if yh < i:
+                yh = i
+    for j in range(col_num):
+        count = 0
+        for i in range(row_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > row_num - row_num_limit:
+            if xl > j:
+                xl = j
+            if xr < j:
+                xr = j
+    return xl, xr, yh, yl
+
 def img_findContours(img_contours,oldimg):
     img, contours, hierarchy = cv2.findContours(img_contours, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > Min_Area]
+    #contours = [cnt for cnt in contours if cv2.contourArea(cnt) > Min_Area]
     print("findContours len = ", len(contours))
     # 排除面积最小的点
-
+    debug.img_show(img)
     car_contours = []
     for cnt in contours:
+
         ant = cv2.minAreaRect(cnt)
         width, height = ant[1]
-
         if width < height:
             width, height = height, width
-
         ration = width / height
         print(ration)
         if ration > 2 and ration < 5.5:
             car_contours.append(ant)
             box = cv2.boxPoints(ant)
             box = np.int0(box)
-            # oldimg = cv2.drawContours(oldimg, [box], 0, (0, 0, 255), 2)
-            # cv2.imshow("edge4", oldimg)
-            # cv2.waitKey(0)
+            debug.img_contours(oldimg,box)
     return  car_contours
 
 def img_Transform(car_contours,oldimg,pic_width,pic_hight):
     car_imgs = []
-
     for car_rect in car_contours:
         if car_rect[2] > -1 and car_rect[2] < 1:
             angle = 1
             # 对于角度为-1 1之间时，默认为1
         else:
             angle = car_rect[2]
-
         car_rect = (car_rect[0], (car_rect[1][0] + 5, car_rect[1][1] + 5), angle)
         box = cv2.boxPoints(car_rect)
         heigth_point = right_point = [0, 0]
@@ -80,8 +115,7 @@ def img_Transform(car_contours,oldimg,pic_width,pic_hight):
             point_limit(left_point)
             car_img = dst[int(left_point[1]):int(heigth_point[1]), int(left_point[0]):int(new_right_point[0])]
             car_imgs.append(car_img)
-            cv2.imshow("card", car_img)
-            cv2.waitKey(0)
+            debug.img_show(car_img)
         elif left_point[1] > right_point[1]:  # 负角度
             new_left_point = [left_point[0], heigth_point[1]]
             pts2 = np.float32([new_left_point, heigth_point, right_point])  # 字符只是高度需要改变
@@ -93,8 +127,7 @@ def img_Transform(car_contours,oldimg,pic_width,pic_hight):
             point_limit(new_left_point)
             car_img = dst[int(right_point[1]):int(heigth_point[1]), int(new_left_point[0]):int(right_point[0])]
             car_imgs.append(car_img)
-            cv2.imshow("card", car_img)
-            cv2.waitKey(0)
+            debug.img_show(car_img)
     return car_imgs
 
 def img_color(card_imgs):
@@ -113,11 +146,11 @@ def img_color(card_imgs):
                 H = card_img_hsv.item(i, j, 0)
                 S = card_img_hsv.item(i, j, 1)
                 V = card_img_hsv.item(i, j, 2)
-                if 11 < H <= 34 and S > 34:  # 图片分辨率调整
+                if 11 < H <= 34 and S > 34:
                     yello += 1
-                elif 35 < H <= 99 and S > 34:  # 图片分辨率调整
+                elif 35 < H <= 99 and S > 34:
                     green += 1
-                elif 99 < H <= 124 and S > 34:  # 图片分辨率调整
+                elif 99 < H <= 124 and S > 34:
                     blue += 1
 
                 if 0 < H < 180 and 0 < S < 255 and 0 < V < 46:
@@ -141,12 +174,11 @@ def img_color(card_imgs):
             limit2 = 124  # 有的图片有色偏偏紫
         elif black + white >= card_img_count * 0.7:
             color = "bw"
-        print(color)
+        #print(color)
         colors.append(color)
-        # print(blue, green, yello, black, white, card_img_count)
-        # cv2.imshow("color", card_img)
-        # cv2.waitKey(0)
+        card_imgs[card_index] = card_img
         if limit1 == 0:
             continue
 
-    return  colors
+
+    return  colors,card_imgs
