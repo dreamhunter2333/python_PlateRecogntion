3249ee9a69ab24b6392a4e15528ef107563eda3e
yinghualuowu <1048146365@qq.com>
2018-04-16 11:05:16 +0800

    用于调试信息

--- debug.py
+++ debug.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 __author__ = '樱花落舞'
+import cv2
+
+def img_show(filename):
+    cv2.imshow("img_show",filename)
+    cv2.waitKey(0)
+
+def img_contours(oldimg,box):
+    oldimg = cv2.drawContours(oldimg, [box], 0, (0, 0, 255), 2)
+    cv2.imshow("img_contours", oldimg)
+    cv2.waitKey(0)
+
