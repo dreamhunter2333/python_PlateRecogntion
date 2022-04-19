1bfd82b7585f9d1de222fa48192c66c3c53685e0
Dream Hunter <dreamhunter2333@gmail.com>
2022-04-19 21:13:53 +0800

    feat: add port support for search sql (#37)

--- search_sql.py
+++ search_sql.py
@@ -1,134 +1,130 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-__author__ = 'jinmu333'
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter.filedialog import *
-import tkinter.messagebox
 import pymysql
+import tkinter as tk
+import tkinter.messagebox
+
+from tkinter import END, LEFT, RIGHT, TOP, StringVar, Text, ttk
 
 
 class Search(ttk.Frame):
     def __init__(self, win):
         ttk.Frame.__init__(self, win)
-        frame00 = ttk.Frame(self)
-        frame0 = ttk.Frame(self)
-        frame1 = ttk.Frame(self)
-        frame2 = ttk.Frame(self)
-        frame3 = ttk.Frame(self)
-        frame4 = ttk.Frame(self)
-        frame5 = ttk.Frame(self)
-        frame6 = ttk.Frame(self)
+
         win.title("车牌数据库查询")
-        # win.minsize(920, 600)
-        self.s1 = StringVar()
-        self.s2 = StringVar()
-        self.s3 = StringVar()
-        self.s4 = StringVar()
-        self.s5 = StringVar()
-        self.s6 = StringVar()
 
-        frame00.pack(side=TOP, fill=tk.Y, expand=1)
-        frame0.pack(side=TOP, fill=tk.Y, expand=1)
-        frame1.pack(side=TOP, fill=tk.Y, expand=1)
-        frame2.pack(side=TOP, fill=tk.Y, expand=1)
-        frame3.pack(side=TOP, fill=tk.Y, expand=1)
-        frame4.pack(side=TOP, fill=tk.Y, expand=1)
-        frame6.pack(side=TOP, fill=tk.Y, expand=1)
-        frame5.pack(side=TOP, fill=tk.Y, expand=1)
+        self.host = StringVar()
+        self.port = StringVar()
+        self.user = StringVar()
+        self.passwd = StringVar()
+        self.db = StringVar()
+        self.table = StringVar()
+        self.keyword = StringVar()
 
-        self.t = Text(frame00, width=100)
-        self.t.pack()
-        self.t.insert('1.0', "此处显示查询结果\n")
+        frame_result = ttk.Frame(self)
+        frame_host = ttk.Frame(self)
+        frame_user = ttk.Frame(self)
+        frame_input = ttk.Frame(self)
+        frame_passwd = ttk.Frame(self)
+        frame_db = ttk.Frame(self)
+        frame_table = ttk.Frame(self)
+        frame_clean = ttk.Frame(self)
+        frame_keyword = ttk.Frame(self)
 
-        self.label0 = ttk.Label(frame0, text='数据库地址：', width=10)
-        self.label0.pack(side=LEFT)
-        self.input0 = ttk.Entry(frame0, textvariable=self.s1, width=30)
-        self.input0.pack(side=RIGHT)
+        frame_result.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_host.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_input.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_user.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_passwd.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_db.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_table.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_keyword.pack(side=TOP, fill=tk.Y, expand=1)
+        frame_clean.pack(side=TOP, fill=tk.Y, expand=1)
 
-        self.label = ttk.Label(frame1, text='用户名：', width=10)
-        self.label.pack(side=LEFT)
-        self.input1 = ttk.Entry(frame1, textvariable=self.s2, width=30)
-        self.input1.pack(side=RIGHT)
+        self.result = Text(frame_result, width=100)
+        self.result.pack()
+        self.result.insert('1.0', "此处显示查询结果\n")
 
-        self.label2 = ttk.Label(frame2, text='密码: ', width=10)
-        self.label2.pack(side=LEFT)
-        self.input2 = ttk.Entry(frame2, textvariable=self.s3, width=30)
-        self.input2.pack(side=RIGHT)
+        self.host_label = ttk.Label(frame_host, text='数据库地址：', width=10)
+        self.host_label.pack(side=LEFT)
+        self.host_input = ttk.Entry(
+            frame_host, textvariable=self.host, width=30)
+        self.host_input.pack(side=RIGHT)
 
-        self.label3 = ttk.Label(frame3, text='数据库名称: ', width=10)
-        self.label3.pack(side=LEFT)
-        self.input3 = ttk.Entry(frame3, textvariable=self.s4, width=30)
-        self.input3.pack(side=RIGHT)
+        self.port_lable = ttk.Label(frame_input, text='端口号', width=10)
+        self.port_lable.pack(side=LEFT)
+        self.port_input = ttk.Entry(
+            frame_input, textvariable=self.port, width=30)
+        self.port_input.pack(side=RIGHT)
 
-        self.label4 = ttk.Label(frame4, text='数据表名称: ', width=10)
-        self.label4.pack(side=LEFT)
-        self.input4 = ttk.Entry(frame4, textvariable=self.s5, width=30)
-        self.input4.pack(side=RIGHT)
+        self.user_label = ttk.Label(frame_user, text='用户名：', width=10)
+        self.user_label.pack(side=LEFT)
+        self.user_input = ttk.Entry(
+            frame_user, textvariable=self.user, width=30)
+        self.user_input.pack(side=RIGHT)
 
-        self.input5 = ttk.Entry(frame6, textvariable=self.s6, width=30)
-        self.input5.pack(side=LEFT)
-        self.label5 = ttk.Button(frame6, text='关键字查询', width=10, command=self.sql2)
-        self.label5.pack(side=RIGHT)
+        self.passwd_label = ttk.Label(frame_passwd, text='密码: ', width=10)
+        self.passwd_label.pack(side=LEFT)
+        self.passwd_input = ttk.Entry(
+            frame_passwd, textvariable=self.passwd, width=30)
+        self.passwd_input.pack(side=RIGHT)
 
-        self.clean_button = ttk.Button(frame5, text="清楚输入信息", width=15, command=self.clean)
+        self.db_label = ttk.Label(frame_db, text='数据库名称: ', width=10)
+        self.db_label.pack(side=LEFT)
+        self.db_input = ttk.Entry(frame_db, textvariable=self.db, width=30)
+        self.db_input.pack(side=RIGHT)
+
+        self.table_label = ttk.Label(frame_table, text='数据表名称: ', width=10)
+        self.table_label.pack(side=LEFT)
+        self.table_input = ttk.Entry(
+            frame_table, textvariable=self.table, width=30)
+        self.table_input.pack(side=RIGHT)
+
+        self.keyword_input = ttk.Entry(
+            frame_keyword, textvariable=self.keyword, width=30)
+        self.keyword_input.pack(side=LEFT)
+        self.keyword_button = ttk.Button(
+            frame_keyword, text='关键字查询', width=10, command=self.keyword_search)
+        self.keyword_button.pack(side=RIGHT)
+
+        self.clean_button = ttk.Button(
+            frame_clean, text="清除输入信息", width=15, command=self.clean)
         self.clean_button.pack(side=LEFT)
-        self.url_face_button = ttk.Button(frame5, text="开始查询", width=15, command=self.sql)
-        self.url_face_button.pack(side=LEFT)
+        self.search_button = ttk.Button(
+            frame_clean, text="开始查询", width=15, command=self.search)
+        self.search_button.pack(side=LEFT)
 
         self.pack(fill=tk.BOTH, expand=tk.YES, padx="10", pady="10")
 
         self.center_window()
 
-    def sql(self):
-        self.t.delete(1.0, END)
-        NAME1 = self.input0.get() or "localhost"
-        USRE1 = self.input1.get() or "python"
-        PASS1 = self.input2.get() or "Python12345@"
-        SQLNAME1 = self.input3.get() or "chepai"
-        TABLENAME1 = self.input4.get() or "CARINFO"
-        self.select_sql(NAME1, USRE1, PASS1, SQLNAME1, TABLENAME1)
-
-    def sql2(self):
-        self.t.delete(1.0, END)
-        NAME1 = self.input0.get() or "localhost"
-        USRE1 = self.input1.get() or "python"
-        PASS1 = self.input2.get() or "Python12345@"
-        SQLNAME1 = self.input3.get() or "chepai"
-        TABLENAME1 = self.input4.get() or "CARINFO"
-        CARPLA1 = self.input5.get()
-        if (CARPLA1==""):
-            tkinter.messagebox.showinfo(title='车牌数据库系统', message='关键字不能为空')
-            return
-        CARPLA1 = "%" + CARPLA1 + "%"
-        self.select_sql2(NAME1, USRE1, PASS1, SQLNAME1, TABLENAME1, CARPLA1)
-
-    def clean(self):
-        self.s1.set("")
-        self.s2.set("")
-        self.s3.set("")
-        self.s4.set("")
-        self.s5.set("")
-        self.s6.set("")
-        self.t.delete(1.0, END)
-
-    def select_sql(self, NAME, USRE, PASS, SQLNAME, TABLENAME):
-        # 打开数据库连接
+    def get_db(self):
         try:
             # 打开数据库连接
-            db = pymysql.connect(NAME, USRE, PASS, SQLNAME)
-        except:
-            print("数据库连接失败")
-            self.t.insert('1.0', "数据库连接失败")
+            return pymysql.connect(
+                host=self.host_input.get() or "localhost",
+                port=int(self.port_input.get() or "3306"),
+                user=self.user_input.get() or "python",
+                passwd=self.passwd_input.get() or "Python12345@",
+                database=self.db_input.get() or "chepai"
+            )
+        except Exception as e:
+            print("数据库连接失败", e)
             return
 
-        # 使用cursor()方法获取操作游标
+    def search(self):
+        self.result.delete(1.0, END)
+        table = self.table_input.get() or "CARINFO"
+        # 打开数据库连接
+        db = self.get_db()
+        if not db:
+            self.result.insert('1.0', "数据库连接失败")
+            return
+
         cursor = db.cursor()
 
         # SQL 查询语句
-        sql = "SELECT * FROM %s" % (TABLENAME)
-        # print(sql)
+        sql = "SELECT * FROM %s" % (table)
         try:
             # 执行SQL语句
             cursor.execute(sql)
@@ -136,36 +132,41 @@ class Search(ttk.Frame):
             results = cursor.fetchall()
             for row in results:
                 row2 = str(row) + "\n"
-                self.t.insert('1.0', row2)
-                self.t.insert('1.0', "-----------------------------"
-                                     "------------------------------"
-                                     "------------------------------"
-                                     "--------\n")
+                self.result.insert('1.0', row2)
+                self.result.insert('1.0', "-----------------------------"
+                                   "------------------------------"
+                                   "------------------------------"
+                                   "--------\n")
                 # print(row)
             # print(results)
-        except:
-            return 0
+        except Exception as e:
+            print(e)
+            self.result.insert('1.0', "查询失败" + e)
 
-        # 关闭数据库连接
         db.close()
 
-    def select_sql2(self, NAME, USRE, PASS, SQLNAME, TABLENAME, CARPLA):
-        # 打开数据库连接
-        try:
-            # 打开数据库连接
-            db = pymysql.connect(NAME, USRE, PASS, SQLNAME)
-        except:
-            print("数据库连接失败")
-            self.t.insert('1.0', "数据库连接失败")
+    def keyword_search(self):
+        self.result.delete(1.0, END)
+        table = self.table_input.get() or "CARINFO"
+        keyword = self.keyword_input.get()
+
+        if (keyword == ""):
+            tkinter.messagebox.showinfo(title='车牌数据库系统', message='关键字不能为空')
             return
 
-        # 使用cursor()方法获取操作游标
+        keyword = "%" + keyword + "%"
+        # 打开数据库连接
+        db = self.get_db()
+        if not db:
+            self.result.insert('1.0', "数据库连接失败")
+            return
+
         cursor = db.cursor()
 
         # SQL 查询语句
         sql = "SELECT * FROM %s WHERE TEXT1 like ('%s') or TEXT2 like ('%s') " \
               "or API like ('%s') or COLOR1 like ('%s') or COLOR2 like ('%s')" \
-                % (TABLENAME, CARPLA, CARPLA, CARPLA, CARPLA, CARPLA)
+            % (table, keyword, keyword, keyword, keyword, keyword)
         # print(sql)
         try:
             # 执行SQL语句
@@ -174,18 +175,27 @@ class Search(ttk.Frame):
             results = cursor.fetchall()
             for row in results:
                 row2 = str(row) + "\n"
-                self.t.insert('1.0', row2)
-                self.t.insert('1.0', "-----------------------------"
-                                     "------------------------------"
-                                     "------------------------------"
-                                     "--------\n")
+                self.result.insert('1.0', row2)
+                self.result.insert('1.0', "-----------------------------"
+                                   "------------------------------"
+                                   "------------------------------"
+                                   "--------\n")
                 # print(row)
             # print(results)
-        except:
-            return 0
-
+        except Exception as e:
+            print(e)
+            self.result.insert('1.0', "查询失败" + e)
         # 关闭数据库连接
         db.close()
+
+    def clean(self):
+        self.host.set("")
+        self.user.set("")
+        self.passwd.set("")
+        self.db.set("")
+        self.table.set("")
+        self.keyword.set("")
+        self.result.delete(1.0, END)
 
     def center_window(self):
         screenwidth = search.winfo_screenwidth()
@@ -204,12 +214,8 @@ def close_window():
 
 if __name__ == '__main__':
     search = tk.Tk()
-
-    search2 = Search(search)
+    search_window = Search(search)
     # close,退出输出destroy
     search.protocol('WM_DELETE_WINDOW', close_window)
     # 进入消息循环
     search.mainloop()
-
-
-
