89b9269a55b856e86852752db3c1a714be01d8b2
Dream Hunter <dreamhunter2333@gmail.com>
2023-05-09 22:23:11 +0800

    feat: update how to config .env (#80)

--- lib/config.py
+++ lib/config.py
@@ -4,14 +4,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    api_id: str
-    api_key: str
-    api_secret_key: str
-    host: str
-    port: int
-    dbuser: str
-    passwd: str
-    database: str
+    api_id: str = "15777797"
+    api_key: str = "xkQmQk08d7pTP56LqXhqpUbm"
+    api_secret_key: str = "bzgSQwTy6WTkXczLlYPfOwu2OQZQ8CEg"
+    host: str = "localhost"
+    port: int = 3306
+    dbuser: str = "python"
+    passwd: str = "Python12345@"
+    database: str = "chepai"
 
     class Config:
         env_file = os.environ.get("ENV_FILE", ".env")
